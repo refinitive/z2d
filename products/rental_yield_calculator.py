5a565cb1ea4d6b8b77f7cf18c6c6fba059f8cbd6
Tallon Robertson <262371891+refinitive@users.noreply.github.com>
2026-02-19 11:12:58 +0200

    change to Net Yield calculation by including operating expenses

--- products/rental_yield_calculator.py
+++ products/rental_yield_calculator.py
@@ -1,18 +1,23 @@
-print("Gross Rental Yield Calculator")
-print("-----------------------")
+print("Net Rental Yield Calculator")
+print("---------------------------")
 
 # Get inputs
 purchase_price = float(input("Enter purchase price: "))
 monthly_rent = float(input("Enter monthly rent: "))
+monthly_expenses = float(input("Enter monthly operating expenses: "))
 
 # Calculate
 annual_rent = monthly_rent * 12
+annual_expenses = monthly_expenses * 12
+net_operating_income = annual_rent - annual_expenses
 yield_percentage = (annual_rent / purchase_price) * 100
 
 # Output
 print("\nResults")
 print("Annual Rent:", annual_rent)
-print("Gross Yield:", round(yield_percentage, 2), "%")
+print("Annual Expenses:", annual_expenses)
+print("Net Operating Income:", net_operating_income)
+print("Net Yield:", round(yield_percentage, 2), "%")
 
 # Rating
 if yield_percentage > 10:
@@ -21,4 +26,5 @@ elif yield_percentage >= 7:
     print("Rating: Moderate deal")
 else:
     print("Rating: Weak deal")
-    
+
+print("---------------------------")
